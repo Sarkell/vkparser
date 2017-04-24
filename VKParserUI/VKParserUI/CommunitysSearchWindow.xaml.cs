1150ba690c33105ced748696bde8f68c7e1f9440
Oleksandr_Breslavets <Oleksandr_Breslavets@epam.com>
2017-04-24 21:43:50 +0300

    Заимплеменчены основные 4-е окна из макета

---
+++ VKParserUI/VKParserUI/CommunitysSearchWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VKParserUI
+{
+    /// <summary>
+    /// Логика взаимодействия для CommunitysSearchWindow.xaml
+    /// </summary>
+    public partial class CommunitysSearchWindow : Window
+    {
+        public CommunitysSearchWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                Rect bounds = Properties.Settings.Default.WindowPosition;
+                this.Top = bounds.Top;
+                this.Left = bounds.Left;
+                // Восстановить размеры, только если они
+                // устанавливались для окна вручную
+                if (this.SizeToContent == SizeToContent.Manual)
+                {
+                    this.Width = bounds.Width;
+                    this.Height = bounds.Height;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Нет сохраненных параметров");
+            }
+        }
+
+        private void button_Authorization_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorizationWindow a = new AuthorizationWindow();
+            a.Show();
+            this.Close();
+        }
+          
+        private void button_Like_Repost_Click(object sender, RoutedEventArgs e)
+        {
+            LikeRepostWindow a = new LikeRepostWindow();
+            a.Show();
+            this.Close();
+        }
+
+        private void button_Communities_Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ VKParserUI/VKParserUI/SMSWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VKParserUI
+{
+    /// <summary>
+    /// Логика взаимодействия для SMSWindow.xaml
+    /// </summary>
+    public partial class SMSWindow : Window
+    {
+        public SMSWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text != "")
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста введите пароль из смс");
+            }
+        }
+    }
+}
